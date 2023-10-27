2a5209de04bccc7911f0acb16d0b9bbdf4c131fc
KamruzzamanAsif <asif720karmuzzaman@gmail.com>
2023-10-27 10:03:27 +0600

    sample code folder deleted

--- sample-code/examples/1.py
+++
@@ -1,7 +0,0 @@
-def foo(x, y):
-    a = x - bar(y)
-    for i in range(a):
-        x += i
-    z = bar(x) - y
-    y = 5
-    return x * y
--- sample-code/examples/2.py
+++
@@ -1,5 +0,0 @@
-def xxx(a, b, c):
-    a = b
-    a = c + a
-    a = a + a
-    return a + b + c
